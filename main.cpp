18ce2a1d13f2d473097cee18b098ac2dac01c7ee
xdexst <rnoy10303@gmail.com>
2024-03-04 00:45:20 +0400

    Установка значений In1 и In2 для объекта logElement и вывод значений на экран

--- main.cpp
+++ main.cpp
@@ -5,3 +5,8 @@ int main() {
     using namespace std;
     cout << "Test v 5.011: OK\n";
     LogElement logElement;
+	 logElement.setIn1(true);
+    logElement.setIn2(false);
+    cout << boolalpha;
+    cout << "In1:" << logElement.getIn1() << endl;
+    cout << "In2:" << logElement.getIn2() << endl
